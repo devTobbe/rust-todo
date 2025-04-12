666221e96f19fd6414e3b504895a659c914dd0ef
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-04-12 16:33:45 +0200

    feat: add args handling

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
-use std::io;
-
+mod command;
 mod todo;
+
+use clap::Parser;
+use command::{Cli, Command};
+use std::io;
 
 fn main() {
     println!("Welcome to the RUSTY TODO-LIST");
     // TODO: Add storage loading here later...
     let mut todos = todo::Todos::new();
 
-    loop {
-        println!("What do you wanna do?");
+    let cli = Cli::parse();
 
-        let mut choice = String::new();
-
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Failed to read line");
-
-        // TODO: Replace this with options handling
-        match choice.as_str().trim() {
-            "create" => create_opt(String::from("Test"), &mut todos) ,
-            "edit" => println!("Here is the EDIT TODO function"),
-            "delete" => println!("{:?}", todos.delete(0)),
-            "toggle" => println!("{:?}", todos.toggle(1)),
-            "list" => todos.list(),
-            _ => println!(
-                "Try using the following: create, edit, delete,
-                toggle, list"
-            ),
-        };
+    match cli.command {
+        Command::Create { title } => {
+            todos.add(title);
+        }
+        Command::Delete { index } => {
+            let _ = todos.delete(index);
+        }
+        Command::Toggle { index } => {
+            let _ = todos.toggle(index);
+        }
+        Command::Edit { index, title }  => {
+            todos.edit(index, title)
+        }
+        Command::List => {
+            todos.list()
+        }
     }
-}
-
-fn create_opt(title : String,  t : &mut todo::Todos) {
-    t.add(title);
 }
